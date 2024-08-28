16802e4582e1e38ee01dba8269aaa3aa383b8170
Joao Guilherme <joaoguilhermeferreira19@gmail.com>
2024-08-28 11:57:13 -0300

    slider inicio

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')]
 const preBtn = [...document.querySelectorAll('.pre-btn')]
 
+//slider cards
 productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
@@ -14,3 +15,46 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+//slider imagens
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+
+
+document.addEventListener("DOMContentLoaded", initializeSlider());
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    if (index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex --;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    clearInterval(intervalId);
+    slideIndex++;
+    showSlide(slideIndex);
+}
