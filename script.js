b1e4f4fe5f74f8c791270257af8267eddd05be08
Joao Guilherme <joaoguilhermeferreira19@gmail.com>
2024-08-29 18:40:47 -0300

    secao sobre

--- script.js
+++ script.js
@@ -22,14 +22,12 @@ const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null;
 
-
-
 document.addEventListener("DOMContentLoaded", initializeSlider());
 
 function initializeSlider(){
     if(slides.length > 0){
         slides[slideIndex].classList.add("displaySlide");
-        intervalId = setInterval(nextSlide, 5000);
+        intervalId = setInterval(nextSlide, 4000);
     }
 }
 
@@ -48,13 +46,11 @@ function showSlide(index){
 }
 
 function prevSlide(){
-    clearInterval(intervalId);
     slideIndex --;
     showSlide(slideIndex);
 }
 
 function nextSlide(){
-    clearInterval(intervalId);
     slideIndex++;
     showSlide(slideIndex);
 }
